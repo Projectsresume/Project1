68290d5dccae765de366830ad4b94c825ef2b820
Alexander Anderson <124541708+Projectsresume@users.noreply.github.com>
2023-08-19 15:48:47 -0400

    Update MainPage.java

--- src/test/java/MainPage.java
+++ src/test/java/MainPage.java
@@ -3,6 +3,7 @@ import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+
 public class MainPage extends BasePage {
     public MainPage(WebDriver driver) {
         super(driver);
