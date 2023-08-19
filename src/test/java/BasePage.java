fcff581dc38ec1f01e4afc9178fa6519a3919907
Alexander Anderson <124541708+Projectsresume@users.noreply.github.com>
2023-08-19 11:00:05 -0400

    Update BasePage.java

--- src/test/java/BasePage.java
+++ src/test/java/BasePage.java
@@ -1,8 +1,8 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.time.Duration;
+
 public class BasePage {
     protected WebDriver driver;
     protected Wait<WebDriver> wait;
